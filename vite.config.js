95f278ec6c6b6e905445f4c716ffc8c35b9e5a28
klay <ads1029384756@gmail.com>
2023-03-15 00:23:28 +0800

    gh-pages restore

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 //https://vitejs.dev/config/
 export default defineConfig({
   base:'/DamageCalculator/',
-  plugins: [vue()]
+  plugins: [vue()],
+  filenameHashing: false
 })
